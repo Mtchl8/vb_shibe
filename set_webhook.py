6a6b1c8df097da00c8e6e1cfcaa9517ba78311ee
Mtchl8 <37140509+Mtchl8@users.noreply.github.com>
2020-04-11 10:48:11 +0300

    s

--- set_webhook.py
+++ set_webhook.py
@@ -3,8 +3,8 @@ from viberbot import Api
 from viberbot.api.bot_configuration import BotConfiguration
 
 bot_configuration = BotConfiguration(
-    name='MyLearnEnglishBot4',
-    avatar='http://viber.com/avatar.jpg',
+    name='EngShibe',
+    avatar='https://previews.123rf.com/images/greenoptix/greenoptix1904/greenoptix190400022/123442578-illustration-shiba-inu-for-all-dog-owners-what-you-love-about-his-dog-puppy-dog-%C3%A2%E2%82%AC%E2%80%B9%C3%A2%E2%82%AC%E2%80%B9eyes-wagging-t.jpg',
     auth_token=TOKEN
 )
 
