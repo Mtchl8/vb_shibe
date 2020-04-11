e893ab3c5b3e27c7ce5d397f165f1404df9abd2b
Mtchl8 <37140509+Mtchl8@users.noreply.github.com>
2020-04-11 10:44:35 +0300

    s

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from viberbot.api.messages import (
 )
 
 #engine = create_engine('sqlite:///mydb.db', echo=True)
-engine = create_engine('postgres://swjfygeaqoszbb:5793a39663698b2ab1f0d17275d4873089de8e76657f7172ccb3c40d810eba67@ec2-176-34-97-213.eu-west-1.compute.amazonaws.com:5432/d6gu081g5pfh3p', echo=True)
+engine = create_engine('postgres://pjnkzjdepbsejw:020908508e47ebd6317f029623231eb3ba17836e6326ee3a3ce6dea900bf741c@ec2-54-195-247-108.eu-west-1.compute.amazonaws.com:5432/d8ug54bgore9n7', echo=True)
 Base = declarative_base()
 Session = sessionmaker(engine)
 
